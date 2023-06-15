35bef3cb3acb555d3d9bb0c3a6ed815ab5d141b2
Egemen Nair <nairegemen@gmail.com>
2023-06-15 12:51:17 +0300

    Client-Server connection created for login, starting working on jwt.

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import User from '../models/User';
 
 export const createUser = async (req: Request, res: Response) => {
@@ -19,9 +20,15 @@ export const createUser = async (req: Request, res: Response) => {
 export const logUser = async (req: Request, res: Response) => {
   try {
     const { email, password } = req.body;
-    const user = await User.findOne({ email, password });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ status: 'error', user: false });
+    }
     if (user) {
-      return res.json({ status: 'ok', user: true });
+      const same = await bcrypt.compare(password, user.password);
+      if (same) {
+        return res.json({ status: 'ok', user: true });
+      }
     } else {
       return res.json({ status: 'error', user: false });
     }
